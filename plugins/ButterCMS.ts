b4e40c0bfe74810a2a22372000f9498f3e7c6be4
gerzitom <tomas.gerzicak@gmail.com>
2022-05-26 21:10:49 +0200

    preview mode fixed

--- plugins/ButterCMS.ts
+++ plugins/ButterCMS.ts
@@ -7,7 +7,7 @@ export default defineNuxtPlugin(nuxtApp => {
   const config = useRuntimeConfig();
   try {
     const apiKey = String(config.API_KEY);
-    const preview = Boolean(config.PREVIEW);
+    const preview = config.PREVIEW !== "false";
     butterCMS = Butter(apiKey, preview);
   } catch (error) {
     console.error(error);
